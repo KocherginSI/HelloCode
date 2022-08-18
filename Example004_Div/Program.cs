073a62d51678a4b9fda4d20d96b53892a2fbf236
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-18 17:08:02 +0300

    Example004_Div Example004_Div added

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,10 @@
+﻿double numberA = new Random() .Next(1, 10);
+Console.Write("Divident:");
+Console.WriteLine(numberA);
+double numberB = new Random() .Next(1, 10);
+Console.Write("Devisor:");
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.Write("Quotient:");
+Console.WriteLine(result);
+
