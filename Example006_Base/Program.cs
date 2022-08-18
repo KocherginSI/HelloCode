7cf5031a108823da3d064cdb7d12c93860b3dc60
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-18 18:07:45 +0300

    Example006_Base added

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,2 +1,26 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int a = new Random() .Next(1, 10);
+int b = new Random() .Next(1, 10);
+int c = new Random() .Next(1, 10);
+int d = new Random() .Next(1, 10);
+int e = new Random() .Next(1, 10);
+int max = a;
+
+Console.Write("a -- ");
+Console.WriteLine(a);
+Console.Write("b -- ");
+Console.WriteLine(b);
+Console.Write("c -- ");
+Console.WriteLine(c);
+Console.Write("d -- ");
+Console.WriteLine(d);
+Console.Write("e -- ");
+Console.WriteLine(e);
+Console.Write("max -- ");
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine(max);
+
