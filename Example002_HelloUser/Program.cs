211654df0a4c4fe32fa45d6c9d5a174e80c9e929
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-18 12:47:27 +0300

    gitignore_test_3

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Enter your name: ");
+﻿//git commit -m "gitignore_test_3"
+Console.Write("Enter your name: ");
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.WriteLine(username);
