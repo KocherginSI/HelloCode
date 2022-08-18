bbf300f5738f6034c9b56435dc77602f16184a32
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-18 17:44:58 +0300

    Example005_Condition_If_Else added

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,18 @@
+﻿int count = 0;
+Console.Write("Введите Имя: ");
+string user_name = Console.ReadLine();
+
+if(user_name.ToLower() == "павел")
+{
+    count += 1;
+    Console.Write("Здравствуйте, ");
+    Console.Write(user_name);
+    Console.WriteLine("!");
+    Console.Write(count);
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.Write(user_name);
+    Console.Write("! Вы знаете Павла?");
+ }
